b9870f08f8310b8eb69f1d2cb6e997ad8108afbd
Waihon Yew <yewwaihon@gmail.com>
2020-06-07 14:40:44 +0800

    Specify number and boolean types

--- StandardTypes.playground/Contents.swift
+++ StandardTypes.playground/Contents.swift
@@ -1,7 +1,14 @@
 import UIKit
 
+// Type Inferrence
 var str = "Hello, playground"
 str = "Hello, Swift"
 let constStr = str
 // Cannot assign to value: constStr is a let constant
 // constStr = "Hello, world"
+
+// Specify Types
+var nextYear: Int = 0
+var bodyTemp: Float = 0
+var hasPet: Bool = true
+
